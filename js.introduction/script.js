061b44dc4a422bcd3a6b1ac87c591d4f7bef307d
abyan123-alhazen <abyan.ahmad@student.alhazen.academy>
2024-10-11 08:52:57 +0700

    Add files via upload

---
+++ js.introduction/script.js
@@ -0,0 +1,6 @@
+//alert = sebuah function untuk menampilkan gop up
+alert("Selamat Datang");
+alert("ini adalah alert");
+
+//prompt = sebuah function untuk menerima input dari user
+prompt("siapa nama kamu ?");
